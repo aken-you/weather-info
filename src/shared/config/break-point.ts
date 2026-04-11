7aa651d845a3a6c28763b9d7a170b6e8d72f2722
aken-you <kinsk2839@naver.com>
2026-04-12 00:32:01 +0900

    feat: 레이아웃 컴포넌트 생성

---
+++ src/shared/config/break-point.ts
@@ -0,0 +1,4 @@
+export const BREAK_POINT = {
+  mobile: '30rem', // 480px 이하는 모바일로 간주
+  desktop: '30.0625rem', // 481px 이상은 데스크탑으로 간주
+}
---
+++ src/shared/ui/app-layout.tsx
@@ -0,0 +1,62 @@
+import { ReactNode } from 'react'
+
+interface AppLayoutProps {
+  children: ReactNode
+}
+
+export function AppLayout({ children }: AppLayoutProps) {
+  return <div>{children}</div>
+}
+
+interface AppLayoutHeaderProps {
+  children: ReactNode
+  className?: string
+}
+
+export function AppLayoutHeader({ children, className }: AppLayoutHeaderProps) {
+  return (
+    <header
+      className={`desktop:h-18 desktop:px-6 flex h-15 items-center border-b border-b-gray-200 px-4 ${className || ''}`}
+    >
+      {children}
+    </header>
+  )
+}
+
+interface AppLayoutContentProps {
+  children: ReactNode
+  className?: string
+}
+
+export function AppLayoutContent({ children, className }: AppLayoutContentProps) {
+  return <div className={`desktop:flex desktop:h-[calc(100vh-4.5rem)] ${className || ''}`}>{children}</div>
+}
+
+interface AppLayoutMainProps {
+  children: ReactNode
+  className?: string
+}
+
+export function AppLayoutMain({ children, className }: AppLayoutMainProps) {
+  return <main className={`desktop:flex-1 desktop:px-6 desktop:py-7 ${className || ''}`}>{children}</main>
+}
+
+interface AppLayoutSidebarProps {
+  children: ReactNode
+  className?: string
+}
+
+export function AppLayoutSidebar({ children, className }: AppLayoutSidebarProps) {
+  return (
+    <aside className={`min-w-60 border-r border-r-gray-200 bg-gray-50 px-3 py-4 ${className || ''}`}>{children}</aside>
+  )
+}
+
+interface AppLayoutFooterProps {
+  children: ReactNode
+  className?: string
+}
+
+export function AppLayoutFooter({ children, className }: AppLayoutFooterProps) {
+  return <footer className={`fixed right-0 bottom-0 left-0 px-4 pt-2 pb-4 ${className || ''}`}>{children}</footer>
+}
